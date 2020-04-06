b294cdc1558b28f16ce19a979da3aa2150b1d7a5
nickxu <xbxmsn@msn.cn>
2020-04-06 23:20:47 +0800

    use fs.createReadStream instead of fs.readFile

--- staticFileServerTest.js
+++ staticFileServerTest.js
@@ -1,19 +1,38 @@
 var http = require('http');
 var fs = require('fs');
-var base = 'index.html';
+var webRoot = './';
 
 http.createServer(function (request, response) {
-  pathname = base + request.url;
+  var requestPath = request.url;
+  var filePath = requestPath.replace(/^.+[/]/g, '');
+  pathname = webRoot + filePath;
   console.log(pathname);
+  // 使用readfile对于大批量处理请求不适合，会加重系统性能
+  // fs.readFile(base, 'utf8', function (err, data) {
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     response.write(data);
+  //     response.end();
+  //   }
+  // });
 
-  fs.readFile(base, 'utf8', function (err, data) {
-    if (err) {
-      console.error(err)
-    } else {
-      response.write(data);
-      response.end();
-    }
+  response.setHeader('Content-Type', 'text/html');
+
+  // create and pipe readable stream
+  var file = fs.createReadStream(pathname);
+  file.on('open', function () {
+    response.statusCode = 200;
+    file.pipe(response);
   });
+
+  file.on('error', function (err) {
+    response.writeHead(403);
+    response.write('file is missing , or no permission problem');
+    console.log(err);
+  })
+
+
 }).listen(8124);
 
 console.log('Static Server running at 8124');
